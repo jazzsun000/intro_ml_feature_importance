1e59f9e64156f201dab9eb8492197545367623f8
jazzsun10 <psun10@usfca.edu>
2023-11-25 21:57:53 -0800

    add explanation guide

--- feature_importance_app.py
+++ feature_importance_app.py
@@ -10,33 +10,32 @@ import shap
 
 st.title("Feature Importance Demonstrator")
 
+st.write("""
+    Welcome to the Feature Importance Demonstrator. This app showcases different methods of evaluating feature importance 
+    in a regression model. Choose a demonstration from the sidebar to see how feature importance is calculated and visualized.
+""")
+
 # Function to plot regression and calculate p-value
 def plot_regression_and_p_value(X, y):
-    # If X is 1-dimensional, reshape it to 2-dimensional
     if X.ndim == 1:
         X = X[:, np.newaxis]
-    
     slope, intercept, r_value, p_value, std_err = stats.linregress(X[:, 0], y)
-    
     fig, ax = plt.subplots()
     ax.scatter(X[:, 0], y, label="Data")
     ax.plot(X[:, 0], intercept + slope*X[:, 0], 'r', label=f"Fitted line (p-value={p_value:.5f})")
     ax.set_xlabel('Feature Value')
     ax.set_ylabel('Target')
     ax.legend()
-
     return p_value, fig
-    
 
 # Function to plot permutation importance
 def plot_permutation_importance(model, X, y):
     result = permutation_importance(model, X, y, n_repeats=10, random_state=42, n_jobs=2)
     sorted_idx = result.importances_mean.argsort()
-
     fig, ax = plt.subplots()
     ax.boxplot(result.importances[sorted_idx].T, vert=False, labels=np.array(range(X.shape[1]))[sorted_idx])
     ax.set_title("Permutation Importance of Features")
-    ax.set_xlabel("Decrease in model performance (e.g., R^2 score)")
+    ax.set_xlabel("Decrease in model performance")
     ax.set_ylabel("Features (by index)")
     return fig
 
@@ -67,6 +66,12 @@ if option == "Unreliability of P-Values":
     num_samples = st.sidebar.slider("Number of resamples", 5, 100, 20)
     resample_size = st.sidebar.slider("Resample Size (as % of total)", 10, 100, 10)
 
+    st.write("""
+        **Unreliability of P-Values**  
+        This demonstration shows how the p-value of a feature can vary with different subsets of the data. 
+        A low p-value suggests that the feature is significantly associated with the target variable.
+    """)
+
     original_p_value, original_fig = plot_regression_and_p_value(X[:, [0]], y)
     st.pyplot(original_fig)
     st.write(f"Original P-value: {original_p_value:.5f}")
@@ -77,14 +82,8 @@ if option == "Unreliability of P-Values":
         resampled_indices = np.random.choice(range(X.shape[0]), size=int(resample_size / 100 * X.shape[0]), replace=False)
         X_resampled = X[resampled_indices]
         y_resampled = y[resampled_indices]
-
-        # Ensure X_resampled is 2-dimensional
-        if X_resampled.ndim == 1:
-            X_resampled = X_resampled[:, np.newaxis]
-    
         p_value, _ = plot_regression_and_p_value(X_resampled, y_resampled)
         p_values.append(p_value)
-
 
     # Plot the distribution of p-values
     fig, ax = plt.subplots()
@@ -94,16 +93,22 @@ if option == "Unreliability of P-Values":
     ax.set_ylabel('Frequency')
     st.pyplot(fig)
 
-# Show the selected demonstration
 elif option == "Permutation Importance":
-    # Train a random forest regressor
-    model = RandomForestRegressor(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
-
-    # Generate the permutation importance plot
+    st.write("""
+        **Permutation Importance**  
+        This method evaluates the importance of a feature by calculating the increase in the model's prediction error 
+        after permuting the feature. A higher value means the feature is more important for model predictions.
+    """)
+    
     fig = plot_permutation_importance(model, X_test, y_test)
     st.pyplot(fig)
 
 elif option == "Tree Explainers":
+    st.write("""
+        **Tree Explainers (SHAP Values)**  
+        SHAP values explain the contribution of each feature to the prediction of an individual instance. 
+        In this plot, each bar represents the average impact of a feature on the model output.
+    """)
+    
     fig = plot_shap_values(model, X_test)
     st.pyplot(fig)
